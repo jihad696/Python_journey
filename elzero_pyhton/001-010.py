0772428312f4c80a8b826901c774aa24e329a485
Gehad Baleegh <93608888+jihad696@users.noreply.github.com>
2025-03-01 17:01:59 +0200

    elzero_COURSE _ASSIGNMENTS

---
+++ elzero_pyhton/001-010.py
@@ -0,0 +1,24 @@
+'''
+ Multiple Line Comment
+ Python doesn't have multiple line comment but you can use this way
+'''
+
+Name = "Gehad"
+Age = "23"
+Country = "Egypt"
+
+
+print("Name  " + Name + "\n"+ "Age " + Age +  "\n" + "country " + Country )
+
+print("Hello, My Name Is " + Name + " And Iam " + Age +  " Years Old and I Live in" + Country )
+
+
+print(type(Name))
+print(type(Age))
+print(type(Country))
+
+
+
+
+
+
