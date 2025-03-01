0928d041cd925d080286fe674bc44d0bfd97aeed
Gehad Baleegh <93608888+jihad696@users.noreply.github.com>
2025-03-01 20:20:13 +0200

    Create 019-020.py

---
+++ elzero_pyhton/019-020.py
@@ -0,0 +1,33 @@
+#1
+
+print(type(2))
+print(type(0.3))
+print(type(5+4j))
+
+#2
+
+complexNum = 1+2j
+print("The Imaginary Part is : {}".format(complexNum.imag) )
+print("The Real Part is : {}".format(complexNum.real) )
+
+
+#3
+
+num = float(10)
+
+print("this is a float number {:.10f}".format(num))
+print(type(num))
+
+#4
+num2 = int(159.650)
+print(type(num2))
+
+
+#5
+
+print(100 - 115) # = -15
+print(50 * 30)   # = 1500
+print(21 % 4 )   #= 1
+print(int(110 / 11))  #= 10
+print(97 // 20)  #= 4
+
